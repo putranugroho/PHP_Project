ed17aea94249136c1d2c2b0f7de4cdad2152df93
putranugroho <putraa.nugroho@gmail.com>
2021-01-19 21:50:38 +0700

    Update Database

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://project-pnn.herokuapp.com/codeigniter/';
+$config['base_url'] = 'https://project-pnn.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
