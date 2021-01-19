730f2fc8470591c25b051581d0fa35dfce185f28
putranugroho <putraa.nugroho@gmail.com>
2021-01-19 22:38:46 +0700

    Update DB

--- application/helpers/fungsi_helper.php
+++ application/helpers/fungsi_helper.php
@@ -5,7 +5,7 @@ function check_login()
     $ci = &get_instance();
     $user_session = $ci->session->userdata('userid');
     if ($user_session) {
-        redirect('admin/dashboard');
+        redirect('home');
     }
 }
 function check_not_login()
