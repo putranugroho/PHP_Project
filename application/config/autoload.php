efd69f2535078b4db6c338d61cfc6c715afae99a
putranugroho <putraa.nugroho@gmail.com>
2021-01-20 11:44:17 +0700

    Update Model

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('m_mahasiswa', 'm_user');
+$autoload['model'] = array('iso_rpt', 'm_user');
